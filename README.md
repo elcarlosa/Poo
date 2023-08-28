ea046ba628969b84000b7c35f7a92cee940ff55a
elcarlosa <guzmangarciacarlosalfredo@gmail.com>
2023-08-27 18:18:01 -0600

    Actividad3

--- README.md
+++ README.md
@@ -8,3 +8,19 @@
 - _p02_AreaCirculo
 - _p03_AreaTriangulo
 - _p04_PagaTrabajador
+
+## Actividad 2
+- _p05_OperacionesMatematicas
+- _p06_PromedioCalificaciones
+- _p07_OperadoresAsignadas
+- _p08_FuncionesTrigonometricas
+- _p09_DividirEnCifras
+
+## Actividad 3
+
+## Actividad 4
+- _p16_TrabajandoEnteros
+- _p17_TrabajandoFlotantes
+- _p18_TrabajandoConstantes
+- _p19_Trabajandocaracteres
+- _p20_ConversionTipos
---
+++ _p20_ConversionTipos.java
@@ -0,0 +1,20 @@
+public class _p20_ConversionTipos {
+    public static void main(String[] args){
+        int var1 = 9;
+        double var2 = var1;
+        float var3 = 123456.789f;
+        double var4 = var3; // float a double
+
+        System.out.print("\033[H\033[2J"); System.out.flush();
+        System.out.println(String.format("implicita de int a double : %d %f", var1, var2));
+        System.out.println(String.format("Implicita de float a double : %f %f",var3,var4));
+
+        long var5 = 1234567890123456L;
+        int var6 = (int) var5;
+        int var7 = 21448364;
+
+        short var8 = (short) var7;
+        System.out.println(String.format("Manual con casting de long a int : %d %d",var5,var6));
+        System.out.println(String.format("Manual con casting de int a short : %d %d",var7,var8));
+    }
+}
